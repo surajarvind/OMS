8e941fa58fc52cd0fcec90e295ae563f018f8097
Sandipan Seal <sandipan.seal@arvindinternet.com>
2022-09-26 12:44:27 +0530

    Update in Code

--- src/main/java/com/springboot/OMS_POC/Controllers/OrderController.java
+++ src/main/java/com/springboot/OMS_POC/Controllers/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping(value = "/api")
 public class OrderController {
     @Autowired
     private OrderService orderService;
